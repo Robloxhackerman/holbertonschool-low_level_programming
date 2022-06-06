7d10b3d8e3b2a14312af69d3a986b2d6f1b91e2d
root <zeituniannicolas27@gmail.com>
2022-06-06 07:06:09 -0700

    5.0

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but outcome is a piede of art,\n");
+	return (0);
+}
