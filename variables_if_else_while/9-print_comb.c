443456f34313859cbd269392381dbd5780af65fb
root <zeituniannicolas27@gmail.com>
2022-06-07 13:58:51 -0700

    9.2

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,11 +9,14 @@ int main(void)
 {
 	int PEPE;
 
-	for (PEPE = '0'; PEPE < '9'; PEPE++)
+	for (PEPE = '0'; PEPE <= '9'; PEPE++)
 	{
-		putchar(PEPE);
-		putchar(',');
-		putchar(' ');
+		if (PEPE != 9)
+		{
+			putchar(PEPE);
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	printf("\n");
 	return (0);
