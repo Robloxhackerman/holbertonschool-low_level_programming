a1718dd6dfce4d0eaa78db1592b122ec6e1d627b
root <zeituniannicolas27@gmail.com>
2022-06-07 12:41:20 -0700

    2.0

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - algo
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char PEPE;
+
+	for (PEPE = "a"; PEPE <= "z"; PEPE++)
+	{
+		putchar(PEPE);
+	}
+
+	putchar("\n")
+	return (0);
+}
