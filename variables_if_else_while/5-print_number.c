a79341b9f379321e638a0afdbfaf26d926300c98
root <zeituniannicolas27@gmail.com>
2022-06-07 13:19:07 -0700

    5.0

---
+++ variables_if_else_while/5-print_number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - algo
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int PEPE;
+
+	for (PEPE = '0'; PEPE < 10; PEPE++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}
