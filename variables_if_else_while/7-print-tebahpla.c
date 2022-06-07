0eea13632e9f3fe99b418b75a3a766f580133d60
root <zeituniannicolas27@gmail.com>
2022-06-07 13:35:43 -0700

    7.0

---
+++ variables_if_else_while/7-print-tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - algo
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char PEPE;
+
+	for (PEPE = 'z'; PEPE >= 'a'; PEPE--)
+	{
+		putchar(PEPE);
+	}
+
+	putchar('\n');
+	return (0);
+}
