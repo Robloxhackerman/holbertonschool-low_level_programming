ffd0dd19dfd12774d36b3087956212ef4d1b8c1c
root <zeituniannicolas27@gmail.com>
2022-06-07 13:28:18 -0700

    6.0

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - algo
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int PEPE;
+
+	for (PEPE = 0; PEPE < 10; PEPE++)
+	{
+		putchar("%d", PEPE);
+	}
+	putchar('\n');
+	return (0);
+}
