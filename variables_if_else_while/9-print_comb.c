25ae55e4de11e48630cf3bb4f95d44afe2b2b511
root <zeituniannicolas27@gmail.com>
2022-06-07 13:55:01 -0700

    9.0

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - algo
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int PEPE;
+
+	for (PEPE = 0; PEPE < 10; PEPE++)
+	{
+		putchar(PEPE);
+		putchar(", ");
+	}
+	printf("\n");
+	return (0);
+}
