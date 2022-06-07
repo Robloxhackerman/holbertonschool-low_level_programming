bef98f0cca64466164434542c2f70bad1d59360a
root <zeituniannicolas27@gmail.com>
2022-06-07 10:34:40 -0700

    0.8

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,22 +5,27 @@
 /**
  * main - assign a random number to n
  *
- * Return: Always 0 (Success
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
+	{
 		printf("%d is positive\n", n);
+	}
 	else if (n == 0)
+	{
 		printf("%d is zero\n", n);
+	}
 	else if (n < 0)
+	{
 		printf("%d is negative\n", n);
-	return(0);
+	}
+	return (0);
 }
 
 
