314e66e80fcdf0ee93573231c7fbae5a3c593389
root <zeituniannicolas27@gmail.com>
2022-06-07 13:42:41 -0700

    8.1

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,10 +8,15 @@
 int main(void)
 {
 	int PEPE1;
+	char PEPE2;
 
 	for (PEPE = '0'; PEPE <= '9'; PEPE++)
 	{
 		putchar(PEPE1);
+	}
+	for (PEPE2 = 'a'; PEPE >= 'f'; PEPE2++)
+	{
+		putchar(PEPE2);
 	}
 	putchar('\n');
 	return (0);
