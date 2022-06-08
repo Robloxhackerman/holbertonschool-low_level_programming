c9fb935b1525eb727196c80d9b7aec27950431f5
root <zeituniannicolas27@gmail.com>
2022-06-08 12:22:07 -0700

    7.0

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of an int
+ *
+ */
+int print_last_digit(int n)
+{
+	int PEPE;
+
+	PEPE = n % 10;
+
+	_putchar(PEPE);
+	return (n);
+}
