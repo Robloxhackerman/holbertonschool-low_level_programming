962aeb24cb10fc1843f9d96ed596c226a496ded2
root <zeituniannicolas27@gmail.com>
2022-06-08 13:33:33 -0700

    10.0

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - add 2 ints
+ *
+ * Return: return the result of p1 + p2
+ */
+int add (int p1, int p2);
+{
+	int PEPE;
+
+	PEPE = p1 + p2;
+	return (PEPE);
+}
