1b6f552833e8a260e75162bb5ec84b114501dd9e
root <zeituniannicolas27@gmail.com>
2022-06-08 13:37:33 -0700

    10.2

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -3,10 +3,13 @@
 
 /**
  * add - add 2 ints
+ * 
+ * @p1: value to be checked
+ * @p2: value to be checked
  *
  * Return: return the result of p1 + p2
  */
-int add (int p1, int p2)
+int add(int p1, int p2)
 {
 	int PEPE;
 
