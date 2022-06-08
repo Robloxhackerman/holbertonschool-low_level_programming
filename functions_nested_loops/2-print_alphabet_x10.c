0851e73c32a1df5cd0e79e4576040a7499515780
root <zeituniannicolas27@gmail.com>
2022-06-08 09:50:28 -0700

    2.0

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times the alphabet
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char PEPE_C;
+	int PEPE_I
+
+	for (PEPE_I = 0; PEPE_I <= 9; PEPE_I++)
+	{
+		for (PEPE_C = 'a'; PEPE_C <= 'z'; PEPE_C++)
+		{
+			_putchar(PEPE_C);
+		}
+		_putchar('\n');
+	}
+}
