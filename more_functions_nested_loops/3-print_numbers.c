98f2ebcfa2880f258b89883fd3dbdd9215215c35
root <zeituniannicolas27@gmail.com>
2022-06-09 07:03:10 -0700

    23.0

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print - prints numbers from 0 to 9
+ *
+ * Return: return void
+ */
+vpid print_numbers(void)
+{
+	int PEPE;
+
+	for (PEPE = '0'; PEPE >= '9'; PEPE++)
+	{
+		_putchar(PEPE)
+	}
+	_putchar('\n')
+}
