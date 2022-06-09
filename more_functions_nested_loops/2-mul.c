62b7bc2d712cfebf3d3572722030f7f573c7a63a
root <zeituniannicolas27@gmail.com>
2022-06-09 06:57:20 -0700

    2.0

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiplies 2 ints
+ *
+ * @a: value to multiply
+ * @b: idem
+ *
+ * Return: algo
+ */
+int mul(int a, int b)
+{
+	int PEPE;
+
+	PEPE = a * b;
+	return (PEPE);
+}
