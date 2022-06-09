8e2e0b569fe1536a5a1026ed7451e8927c56b93c
root <zeituniannicolas27@gmail.com>
2022-06-09 07:26:26 -0700

    3.8

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: return void
+ */
+void print_numbers(void)
+{
+	int PEPE;
+
+	for (PEPE = 48; PEPE < 58; PEPE++)
+	{
+		if (PEPE != 50 || PEPE != 52)
+		{
+			_putchar(PEPE);
+		}
+	}
+	_putchar('\n');
+}
