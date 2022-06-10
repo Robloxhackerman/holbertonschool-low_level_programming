8963cc590a142f64783268741aeaa21197562ca5
root <zeituniannicolas27@gmail.com>
2022-06-10 06:31:11 -0700

    7.0

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal
+ *
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	int PEPE;
+
+	for (PEPE = 0; n < PEPE; PEPE++)
+	{
+		for (PEPE =; n < PEPE; PEPE++)
+		{
+			_putchar(32);
+		}
+		_putchar(92);
+	}
+	_putchar('\n');
+}
