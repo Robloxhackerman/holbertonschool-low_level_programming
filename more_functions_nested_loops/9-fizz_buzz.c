75c45f7ecfe5b2dddb31be20b53a084958980409
root <zeituniannicolas27@gmail.com>
2022-06-10 07:38:30 -0700

    9.0

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - pepe
+ *
+ * Return: 
+ */
+void main(void)
+{
+	int PEPE;
+
+	for (PEPE = 0; PEPE < 100; PEPE++)
+	{
+		if ((PEPE % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((PEPE % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else if ((PEPE % 3) == 0 && (PEPE % 5) == 0)
+		{
+			printf("Fizz Buzz");
+		}
+		else
+		{
+			printf("%d", PEPE);
+		}
+	}
+	_putchar('\n');
+}
