4f3a97eecf0d00e32863bde56cc7cb1aef3a6b21
root <zeituniannicolas27@gmail.com>
2022-06-10 06:08:58 -0700

    6.0

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - pritn n times line
+ *
+ * Return: nothing
+ */
+void print_line(int n)
+{
+	int PEPE;
+
+	for ( PEPE = 0; PEPE <= n; PEPE++)
+	{
+		if ( n == 0)
+		{
+			_putchar(92 + 110)
+		}
+		else
+		{
+			_putchar(95)
+		}
+	}
+}
