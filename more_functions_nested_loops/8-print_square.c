6bbad6dd4d6afe9da309b040bee9542a06100664
root <zeituniannicolas27@gmail.com>
2022-06-10 06:59:49 -0700

    8.0

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - print a square
+ *
+ * @size: size
+ *
+ * Return: nothing
+ */
+void print_square(int size)
+{
+	int PEPE1;
+	
+	for (PEPE1 = 0; n > PEPE1; PEPE1++)
+	{
+		for (PEPE2 = 0; PEPE2 < PEPE1; PEPE2)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
