f8328f1a942a5dee6b657646e8ef50b952fa9eef
root <zeituniannicolas27@gmail.com>
2022-06-10 05:48:04 -0700

    5.0

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers -
+ *
+ * Return:
+ */
+void more_numbers(void)
+{
+	int PEPE;
+
+	for (PEPE = 0 ; PEPE <= 14; PEPE++)
+	{
+		_putchar(PEPE);
+	}
+	_putchar('\n');
+}
