64f81a6277ad2552f9fb630438758dbdbe9c2f7d
root <zeituniannicolas27@gmail.com>
2022-06-14 08:43:59 -0700

    1.0

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - aaa
+ *
+ * @a:
+ * @b:
+ *
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int PEPE;
+
+	*a = PEPE;
+	*a = *b;
+	*b = PEPE;
+}
+
