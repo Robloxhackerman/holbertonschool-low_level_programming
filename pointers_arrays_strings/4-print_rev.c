b8dc40159d441c8406423b07d021de18d93f5abd
root <zeituniannicolas27@gmail.com>
2022-06-14 09:26:01 -0700

    4.0

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ *
+ * @s: string
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int PEPE;
+
+	for (PEPE = 0; *s != '\0'; s++)
+	{
+		PEPE++;
+	}
+	
+	for(; PEPE <= PEPE; PEPE--)
+	{
+		_putchar(s[PEPE]);
+	}
+	_putchar('\n');
+}
