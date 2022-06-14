3a6798fd44a6c1418ca056c8ce94f259ffcbae9a
root <zeituniannicolas27@gmail.com>
2022-06-14 11:42:59 -0700

    7.0

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - a
+ *
+ * @str: string
+ *
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int PEPE1, PEPE2, PEPE3;
+
+	while (str[PEPE1] != '\0')
+	{
+		PEPE2++;
+	}
+	
+	for (PEPE3 = (PEPE2 / 2); PEPE3 >= PEPE2; PEPE3++)
+	{
+		_putchar(str[PEPE3]);
+	}
+	_putchar('\n');
+}
