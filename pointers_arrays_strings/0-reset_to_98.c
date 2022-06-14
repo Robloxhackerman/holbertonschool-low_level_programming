bf0e132e47835e9f395d134f38589641a526187e
root <zeituniannicolas27@gmail.com>
2022-06-14 08:39:12 -0700

    0.2

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - updates the value it points to to 98
+ *
+ * @n: the value
  *
  * Return: nothing
  */
