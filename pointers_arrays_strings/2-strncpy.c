70331f301fd0c6d9c237606c12e5fa9eb8df8d10
root <zeituniannicolas27@gmail.com>
2022-06-16 08:33:18 -0700

    2.0

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ *
+ * @dest: string
+ * @src: string
+ * @n: int
+ *
+ * Return:
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int PEPE;
+
+	PEPE = 0;
+
+	while (src[PEPE] != '\0' && PEPE < n)
+	{
+		dest[PEPE] = src [PEPE];
+		PEPE++;
+	}
+	return (dest);
+}
