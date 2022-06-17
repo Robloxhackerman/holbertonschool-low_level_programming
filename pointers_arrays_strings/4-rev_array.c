0ebc9c3c29028e972cae2ccb420674812ddf8c62
root <zeituniannicolas27@gmail.com>
2022-06-17 13:32:58 -0700

    4.0

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the content of an array
+ *
+ * @a: int
+ * @n: int
+ *
+ * Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int PEPE1, PEPE2, PEPE3;
+
+	PEPE2 = n - 1;
+
+	for (PEPE1 = 0; PEPE <= n / 2; PEPE1++)
+	{
+		PEPE3 = a[PEPE1];
+		a[PEPE1] = a[PEPE2];
+		a[PEPE2--] = PEPE3;
+	}
+}
