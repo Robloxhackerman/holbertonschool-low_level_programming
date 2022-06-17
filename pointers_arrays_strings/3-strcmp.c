11815a5cebb693a80e70d8baa6c043fee1352999
root <zeituniannicolas27@gmail.com>
2022-06-17 12:13:46 -0700

    3.0

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare 2 strings
+ *
+ * @s1: string to compare
+ * @s2: string to compare
+ *
+ * Return: if s1 is less than s2, return 0; if they are equals 0; 
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+
+		s1++;
+		s2++;
+
+	}
+	return (*s1 - *s2)
+}
