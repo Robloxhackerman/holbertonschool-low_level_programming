fe8e2bd1d386fa760f0d5cf3c950cfa4d6c165f0
root <zeituniannicolas27@gmail.com>
2022-06-17 12:37:31 -0700

    3.3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
  * @s1: string to compare
  * @s2: string to compare
  *
- * Return: if s1 is less than s2 return less than0, if they are equals return 0, if s1 is greater than s2, return gretyaer than 0
+ * Return: s1 is less than s2 return -0, are equals return 0,s1 is greater than s2, return +0
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -16,10 +16,8 @@ int _strcmp(char *s1, char *s2)
 		{
 			return (0);
 		}
-
 		s1++;
 		s2++;
-
 	}
 	return (*s1 - *s2);
 }
