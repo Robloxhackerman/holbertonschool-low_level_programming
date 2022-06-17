85c311d7462a4baf2d44d771e972c417040aa695
root <zeituniannicolas27@gmail.com>
2022-06-17 13:45:25 -0700

    4.2

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -12,12 +12,11 @@ void reverse_array(int *a, int n)
 {
 	int PEPE1, PEPE2, PEPE3;
 
-	PEPE2 = n - 1;
-
+	PEPE2 = n
 	for (PEPE1 = 0; PEPE1 <= n / 2; PEPE1++)
 	{
 		PEPE3 = a[PEPE1];
-		a[PEPE1] = a[PEPE2];
-		a[PEPE2--] = PEPE3;
+		a[PEPE1] = a[PEPE2 - PEPE1 -1];
+		a[PEPE2 - PEPE1 - 1] = PEPE3;
 	}
 }
