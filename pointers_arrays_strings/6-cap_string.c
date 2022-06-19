77e44a8aef9b0a36465526b5d1dec454b7e671f2
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-19 14:48:17 -0700

    6.11

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -3,28 +3,28 @@
 /**
  * cap_string - capitalize a string
  *
- * @PEPIN: string
+ * @PEPI: string
  *
  * Return: capi. string
  */
-char *cap_string(char *PEPIN)
+char *cap_string(char *PEPI)
 {
-	int PEPE1, PEPE2;
+	int PEP1, PEP2;
 	char PEPE_C[13] = {' ', '\n', '\t', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
-	for (PEPE1 = 0; PEPIN[PEPE1] != '\0'; PEPE1++)
+	for (PEPE1 = 0; PEPI[PEPE1] != '\0'; PEPE1++)
 	{
-		if (PEPE1 == 0 && PEPIN[PEPE1] >= 97 && PEPIN[PEPE1] <= 122)
+		if (PEPE1 == 0 && PEPI[PEPE1] >= 97 && PEPI[PEPE1] <= 122)
 		{
-			PEPIN[PEPE1] = PEPIN[PEPE1] - 32;
+			PEPI[PEPE1] = PEPI[PEPE1] - 32;
 		}
 		for (PEPE2 = 0; PEPE2 < 13 ; PEPE2++)
 		{
-			if (PEPIN[PEPE1] == PEPE_C[PEPE2])
+			if (PEPI[PEPE1] == PEPE_C[PEPE2])
 			{
-				if (PEPIN[PEPE1 + 1] >= 97 && PEPIN[PEPE1 + 1] <= 122)
+				if (PEPI[PEPE1 + 1] >= 97 && PEPI[PEPE1 + 1] <= 122)
 				{
-					PEPIN[PEPE1 + 1] = PEPIN[PEPE1 + 1] - 32;
+					PEPI[PEPE1 + 1] = PEPI[PEPE1 + 1] - 32;
 				}
 			}
 		}
