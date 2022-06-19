ea5ac2d9dfe3f41f5472c1742a8cb4f3f80228b9
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-19 15:24:17 -0700

    7.0

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encode a string into 1337
+ *
+ * @PEPIN: string to encode
+ *
+ * Return: string encoded
+ */
+char *leet(char *PEPIN)
+{
+	int PEPE1, PEPE2;
+	char *PEPE_C1[10] = {'A','E','O','T','L','a','e','o','t','l'};
+	char *PEPE_C2[10] = {'4','3','0','7','1','4','3','0','7','1'};
+
+	for (PEPE1 = 0; PEPIN[PEPE1] != '\0'; PEPE1++)
+	{
+		for (PEPE2 = 0; PEPE2 < 10; PEPE2++)
+		{
+			if (PEPIN[PEPE1] == PEPE_C1[PEPE2])
+			{
+				PEPIN[PEPE1] = PEPE_C2[PEPE2];
+			}
+		}
+	}
+
+	return (PEPIN);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,3 +16,4 @@ int _atoi(char *s);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *leet(char *);
