7011bfbafea8889f5d6c046466efd537c3f91425
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-19 15:34:39 -0700

    100.0

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encode a tring in rot13
+ *
+ * @PEPEIN: string to encode
+ *
+ * Return: string encoded
+ */
+char *rot13(char *)
+{
+	int PEPE1, PEPE2;
+	char PEPE_C1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char PEPE_C2 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (PEPE1 = 0; PEPIN[PEPE1] != '\0'; PEPE1++)
+	{
+		for (PEPE2 = 0; PEPE_C1[PEPE2]; PEPE2++)
+		{
+			if (PEPIN[PEPE1] == PEPE_C1[PEPE2])
+			{
+				PEPIN[PEPE1] = PEPE_C2[PEPE2];
+			}
+		}
+	}
+
+	return (PEPIN);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -17,3 +17,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
