f1b5d2ef43be977a39f481617ee80b1f22d3a534
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-19 14:36:17 -0700

    6.3

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@
 char *cap_string(char *PEPIN)
 {
 	int PEPE1, PEPE2;
-	
+	char PEPE_C[13] = {' ', '\n', '\t', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
 	for (PEPE1 = 0; PEPIN[PEPE1] != '\0'; PEPE1++)
 	{
@@ -18,8 +18,16 @@ char *cap_string(char *PEPIN)
 		{
 			PEPIN[PEPE1] = PEPIN[PEPE1] - 32;
 		}
+
 		for (PEPE2 = 0; PEPE2 < 13 ; PEPE2++)
 		{
+			if (PEPIN[PEPE1] == PEPE_C[PEPE2])
+			{
+				if(PEPIN[PEPE1] >= 97 && PEPIN[PEPE1] <= 122)
+				{
+					PEPIN[PEPE1] = PEPIN[PEPE1] - 32;
+				}
+			}
 		}
 	}
 
