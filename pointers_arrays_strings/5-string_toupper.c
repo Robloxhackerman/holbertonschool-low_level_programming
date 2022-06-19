511b66662e3cbabe8fc97dafc2b25bb7e2681610
root <zeituniannicolas27@gmail.com>
2022-06-19 13:24:12 -0700

    5.1

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -11,7 +11,7 @@ char *string_toupper(char *PEPIN)
 
 	for (PEPE = 0; PEPIN[PEPE]!= '\0'; PEPE++)
 	{
-		if (PEPEIN[PEPE] >= 97 && PEPIN[PEPE] <= 122)
+		if (PEPIN[PEPE] >= 97 && PEPIN[PEPE] <= 122)
 		{
 			PEPIN[PEPE] = PEPIN[PEPE] - 32;
 		}
