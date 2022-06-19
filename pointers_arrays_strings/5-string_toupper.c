f85ccc1df41a80021ef5dd7ee49755c37bbbf8e1
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-19 14:09:41 -0700

    5.5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -3,13 +3,15 @@
 /**
  * string_toupper - change all lowercase to uppercase
  *
+ * @PEPIN: string
+ *
  * Return: uppercase string
  */
 char *string_toupper(char *PEPIN)
 {
 	int PEPE;
 
-	for (PEPE = 0; PEPIN[PEPE]!= '\0'; PEPE++)
+	for (PEPE = 0; PEPIN[PEPE] != '\0'; PEPE++)
 	{
 		if (PEPIN[PEPE] >= 97 && PEPIN[PEPE] <= 122)
 		{
