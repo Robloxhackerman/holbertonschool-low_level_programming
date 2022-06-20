a9df813f374b77f1801974d314f178f38713529b
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-20 09:09:03 -0700

    3.1

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - gets lengthof a prefix substring
+ *
+ * @s: string to check
+ * @accept: string to check
+ *
+ * Return: num of bytes of s in accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int PEPE1, PEPE2;
+
+	for (PEPE1 = 0; s[PEPE1]; PEPE1++)
+	{
+		for (PEPE2 = 0; accept[PEPE2]; PEPE2++)
+		{
+			if (s[PEPE1] == accept[PEPE2])
+			{
+				break;
+			}
+		}
+
+		if (!accept[PEPE2])
+		{
+			break;
+		}
+	}
+
+	return (PEPE1);
+}	
