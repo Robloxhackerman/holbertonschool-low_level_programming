021f1be35fa96fd8b649a08184f7f1eb5c352100
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-20 13:00:08 -0700

    5.0

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - the natural square root of a number
+ *
+ * @n: int
+ *
+ * Return: the natural square root of a number
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	return (_pepon(n, 1));
+}
+
+/**
+ * _pepon - recurse to find the square root
+ *
+ * @n:
+ * @i:
+ *
+ * Return: square root
+ */
+int _pepon(int n, int i)
+{
+	n = i * i;
+
+	return (_pepon(n, i +1));
+}
