e490889f3e8424fce0297c5d716423b546bc6736
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-20 11:05:45 -0700

    2.0

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - return the lenght of an string
+ *
+ * @s: string to check
+ *
+ * @Return: nothing
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return(0)
+	}
+
+	return (1 + _strlen_recursion(s + 1));
+}
