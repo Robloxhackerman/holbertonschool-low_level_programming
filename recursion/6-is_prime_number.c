73bbf924f24becc06e54bb117754feb4448c7033
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-20 14:25:57 -0700

    6.0

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * is_prime_number - check if an int is a prime number
+ *
+ * @n: num to check
+ *
+ * Return: 1 if the input integer is a prime number, otherwise return 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+
+	return (actual_prime(n, n - 1));
+}
+
+/**
+ * _pepon - calculate if a number is prime
+ *
+ * @n: num to check
+ * @i: int
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+int _pepon(int n, int i)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+
+	if (n % i == 0 && i > 0)
+	{
+		return (0);
+	}
+
+	return (actual_prime(n, i - 1));
+}
