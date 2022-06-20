a835131bf082e296fd52a2e7b97c245776b68ed4
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-20 12:14:45 -0700

    4.0

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - the value of x raised to the power of y
+ *
+ * @x: int
+ * @y: int
+ *
+ * Return: the value of x raised to the power of y.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	
+	return (x * _pow_recursion(x, y -1));
+}
