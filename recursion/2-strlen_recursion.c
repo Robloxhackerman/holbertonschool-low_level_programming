fa837edb0621f9fb08355f9bbd30fb983ed29806
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-20 11:10:00 -0700

    2.22

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -5,13 +5,13 @@
  *
  * @s: string to check
  *
- * @Return: nothing
+ * @Return: lenght
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return(0);
+		return (0);
 	}
 
 	return (1 + _strlen_recursion(s + 1));
