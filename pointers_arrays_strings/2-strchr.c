24a74ffdb381c2c5eabc873df26b3443b7a5f111
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-20 09:00:56 -0700

    2.0

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *  * _strchr - locates a character in a string
+ *   * @s: string to check
+ *    * @c: character to check for
+ *     *
+ *      * Return: pointer to spot in s with c or null
+ *       */
+char *_strchr(char *s, char c)
+{
+	int PEPE;
+
+	for (PEPE = 0; *(s + PEPE); PEPE++)
+	{
+		if (*(s + PEPE) == c)
+		{
+			return (s + PEPE);
+		}
+	}
+
+	if (*(s + PEPE) == c)
+	{
+		return (s + PEPE);
+	}
+
+	return (0);
+}
