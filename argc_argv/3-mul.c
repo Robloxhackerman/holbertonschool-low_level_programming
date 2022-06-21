2a8bc99a2afe1d8e1671f2f57dc64c92efdfd96f
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-21 13:20:32 -0700

    3.0

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - multiplies 2 numbers
+ *
+ * @argc: num of args
+ * @argv: args
+ *
+ * Return: 0 (success), 1 (very bad)
+ */
+int main(int argc, char *argv[])
+{
+	int PEPE;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1)
+	}
+	else
+	{
+		PEPE = argv[1] * argv[2];
+	}
+
+	return (0);
+}
