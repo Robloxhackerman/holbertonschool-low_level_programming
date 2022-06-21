58cf1b9e566553b8d59d9917ca1bc31cb672a702
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-21 15:30:12 -0700

    100.3

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
 	
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	
 	if (PEPE_1 <= 0)
 	{
-		printf("0");
+		printf("0\n");
 		return (1);
 	}
 	else
