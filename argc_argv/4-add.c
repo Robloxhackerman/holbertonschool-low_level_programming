63d2b9a3ce2831c0915a0132aa963f90f1246331
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-21 14:22:59 -0700

    4.2

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -3,46 +3,6 @@
 #include <stdlib.h>
 
 /**
- * _atoi - converts a string to an integer
- *
- * @s: string to be converted
- * Return: the int converted from the string
- */
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-	while (s[len] != '\0')
-		len++;
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-	if (f == 0)
-		return (0);
-	return (n);
-}
-
-/**
  * main - multiplies 2 numbers
  *
  * @argc: num of args
@@ -52,18 +12,25 @@ int _atoi(char *s)
  */
 int main(int argc, char *argv[])
 {
-	int PEPE1, PEPE, PEPE_R;
+	int PEPE1, PEPE2, PEPE_R;
 
 	for (PEPE1 = 0; argc < PEPE1; PEPE1++)
 	{
-		PEPE = atoi(argv[PEPE]);
-		
-		if (PEPE >= 0)
+		for (PEPE2 = 0; argv[PEPE1][PEPE2]; PEPE2++;)
 		{
-			PEPE_R = PEPE_R + PEPE;
+			if (isdigit(argv[PEPE1][PEPE2]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		
 	}
+	
+	for (PEPE1 = 0; argc > PEPE1; PEPE1++)
+	{
+		PEPE_R = atoi(argv[PEPE1]);
+	}
+
 	printf("%d\n", PEPE_R);
 
 	return (0);
