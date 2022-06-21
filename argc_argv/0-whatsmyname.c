887d9b3ac17c225f417cf3d67b465e11363c05f3
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-21 11:53:09 -0700

    0.0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prnits name
+ *
+ * @argc:
+ * @argv:
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+}
