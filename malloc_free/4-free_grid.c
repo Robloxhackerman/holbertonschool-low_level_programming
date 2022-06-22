4c7421792c367989c4a6bc9251899e349b6e2338
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-22 09:15:23 -0700

    4.4

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
  /**
-  * free_grid - frees a 2 dimensional grid previously
+  * free_grid -frees a 2 dimensional grid previously
   * created by your alloc_grid function
   *
   * @grid: grid to free
