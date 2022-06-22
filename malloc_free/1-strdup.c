c7c15d5cf0c148aafde1ecee8db44007cad3897a
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-22 03:18:20 -0700

    1.0

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space
+ * in memory
+ *
+ * @str:
+ *
+ * Return: 
+ */
+char *_strdup(char *str)
+{
+	char *PEPE_A;
+	unsigned int PEPE1, PEPE2;
+
+	PEPE1 = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[PEPE1])
+	{
+		PEPE1++;
+	}
+
+	PEPE_A = malloc(sizeof(char) * PEPE1);
+
+	if (PEPE_A == NULL)
+	{
+		return (NULL);
+	}
+
+	for (PEPE2 = 0; PEPE2 < PEPE1; PEPE2++)
+	{
+		PEPE_A[PEPE2] = str[PEPE2];
+	}
+
+	return (PEPE_A);
+}
