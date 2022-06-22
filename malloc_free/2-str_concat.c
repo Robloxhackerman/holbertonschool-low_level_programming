5eeb3cc43b9dd289a5f96f472df14d6ae869413c
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-22 07:03:33 -0700

    2.17

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,52 +16,31 @@ char *str_concat(char *s1, char *s2)
 
 	PEPE1 = 0;
 	PEPE2 = 0;
-
 	if (s1 == NULL)
-	{
 		s1 = "";
-		PEPE1 = 0;
-	}
 	else
 	{
 		while (s1[PEPE1])
-		{
 			PEPE1++;
-		}
 	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-		PEPE2 = 0;
-	}
 	else
 	{
 		while (s2[PEPE2])
-		{
 			PEPE2++;
-		}
 	}
-
 	PEPE3 = PEPE1 + PEPE2;
-	
 	PEPE_A = malloc((sizeof(char) * PEPE3) + 1);
-
 	if (PEPE_A == NULL)
-	{
 		return (NULL);
-	}
-
 	for (PEPE4 = 0; PEPE4 <= PEPE3; PEPE4++)
 	{
 		if (PEPE4 < PEPE1)
-		{
 			PEPE_A[PEPE4] = s1[PEPE4];
-		}
 		if (PEPE4 < PEPE2)
-		{
 			PEPE_A[PEPE4] = s2[PEPE4];
-		}
 	}
 
 	PEPE_A[PEPE4] = '\n';
