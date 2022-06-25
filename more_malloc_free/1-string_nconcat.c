1641078ba1ff4c9e17bd9f221ba1b0930981cb75
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-25 15:15:41 -0700

    1.7

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,24 +16,49 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int PEPE1, b, c, d;
 
 	if (s1 == 0)
+	{
 		s1 = "";
+	}
 	if (s2 == 0)
+	{
 		s2 = "";
+	}
+	
+	
 	for (PEPE1 = 0; s1[PEPE1] != '\0'; PEPE1++)
+	{
 		;
-	for (b = 0; s2[b] != '\0'; b++)
+	}
+	
+	for (PEPE2 = 0; s2[PEPE2] != '\0'; PEPE2++)
+	{
 		;
-	if (n > b)
-		PEPE_A = malloc((PEPE1 + b + 1) * sizeof(char));
+	}
+	
+	if (n >= PEPE2)
+	{
+		PEPE_A = malloc(((PEPE1 + PEPE2 + 1) * sizeof(char)) + 5);
+	}
 	else
-		PEPE_A = malloc((PEPE1 + n + 1) * sizeof(char));
+	{
+		PEPE_A = malloc(((PEPE1 + n + 1) * sizeof(char)) + 5);
+	}
+	
 	if (PEPE_A == 0)
+	{
 		return (NULL);
-
+	}
+	
 	for (c = 0; c < PEPE1; c++)
+	{
 		PEPE_A[c] = s1[c];
-	for (d = 0; d < n && d < b; d++, c++)
+	}
+	
+	for (d = 0; d < n && d < PEPE2; d++, c++)
+	{
 		PEPE_A[c] = s2[d];
+	}
+
 	PEPE_A[c] = '\0';
 	return (PEPE_A);
 }
