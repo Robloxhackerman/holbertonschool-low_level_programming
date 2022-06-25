2e27871bae4aa6ed60f3176074daa4c28f751a4d
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-25 15:37:20 -0700

    1.15

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-		PEPE_A = malloc(((PEPE1 + n + 1) * sizeof(char)) + 5);
+		PEPE2 = n;
+		PEPE_A = malloc(((PEPE1 + PEPE2 + 1) * sizeof(char)) + 5);
 	}
 	
 	if (PEPE_A == 0)
@@ -42,7 +43,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (PEPE3 = 0; PEPE3 < PEPE1; PEPE3++)
 		PEPE_A[PEPE3] = s1[PEPE3];
 	
-	for (PEPE4 = 0; PEPE4 < n && PEPE4 < PEPE2; PEPE4++)
+	for (PEPE4 = 0; PEPE4 < PEPE2; PEPE4++)
 	{
 		PEPE_A[PEPE3] = s2[PEPE4];
 		PEPE3++;
