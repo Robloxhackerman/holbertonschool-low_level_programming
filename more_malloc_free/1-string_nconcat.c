9be1cc205f09f8de6f858c884db6a6d377f13ea0
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-25 15:09:18 -0700

    1.6

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -4,7 +4,7 @@
 /**
  * string_nconcat - concatenate 2 strings
  *
- * @s1: string 1
+ * @s1: 
  * @s2: string 2
  * @n: num of bytes of s2
  *
@@ -13,54 +13,27 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *PEPE_A;
-	unsigned int PEPE1, PEPE2, PEPE3, PEPE4;
+	unsigned int PEPE1, b, c, d;
 
-	PEPE1 = 0;
-	PEPE2 = 0;
-	
 	if (s1 == 0)
-	{
 		s1 = "";
-	}
-	else
-	{
-		while (s1[PEPE1] != '\0')
-			PEPE1++;
-	}
-	
 	if (s2 == 0)
-	{
 		s2 = "";
-	}
+	for (PEPE1 = 0; s1[PEPE1] != '\0'; PEPE1++)
+		;
+	for (b = 0; s2[b] != '\0'; b++)
+		;
+	if (n > b)
+		PEPE_A = malloc((PEPE1 + b + 1) * sizeof(char));
 	else
-	{
-		while (s2[PEPE2] != '\0')
-		{
-			PEPE2++;
-		}
-	}
-	
-	PEPE3 = PEPE1 + n;
-	PEPE_A = malloc(sizeof(char) * (PEPE3 + 1));
-	
-	if (PEPE_A == NULL)
-	{
+		PEPE_A = malloc((PEPE1 + n + 1) * sizeof(char));
+	if (PEPE_A == 0)
 		return (NULL);
-	}
-	
-	for (PEPE4 = 0; PEPE4 <= PEPE3; PEPE4++)
-	{
-		if (PEPE4 < PEPE1)
-		{
-			PEPE_A[PEPE4] = s1[PEPE4];
-		}
-		
-		if (PEPE4 < PEPE2)
-		{
-			PEPE_A[PEPE4] = s2[PEPE4];
-		}
-	}
 
-	PEPE_A[PEPE4] = '\n';
+	for (c = 0; c < PEPE1; c++)
+		PEPE_A[c] = s1[c];
+	for (d = 0; d < n && d < b; d++, c++)
+		PEPE_A[c] = s2[d];
+	PEPE_A[c] = '\0';
 	return (PEPE_A);
 }
