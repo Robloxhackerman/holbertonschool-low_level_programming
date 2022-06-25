f19b9e96b8286d10e7aca2553220a07a5517b92f
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-25 14:06:05 -0700

    0.0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,23 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * malloc_checked - allocate memory using malloc
+ *
+ * @b:
+ *
+ * Return:
+ */
+void *malloc_checked(unsigned int b)
+{
+	int PEPE;
 
+	PEPE = maloc(sizeof(int) * b);
+
+	if (PEPE == 0);
+	{
+		exit(98);
+	}
+
+	return (PEPE);
+}
