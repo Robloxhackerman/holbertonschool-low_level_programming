493a223a41e8c5340bb955cc0ac6f35b5caf0dcb
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-26 12:43:26 -0700

    100.2

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -22,26 +22,21 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (ptr);
 	}
-
 	if (ptr == 0)
 	{
 		PEPE_N = malloc(sizeof(char) * new_size);
 		return (PEPE_N);
 	}
-
 	if (new_size == 0 && ptr != 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
 	PEPE_N = malloc(sizeof(char) * new_size);
-
 	if (PEPE_N == 0)
 	{
 		return (NULL);
 	}
-
 	if (new_size < old_size)
 	{
 		for (PEPE1 = 0; new_size > PEPE1; PEPE1++)
@@ -50,7 +45,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		}
 		return (PEPE_N);
 	}
-	
 	free(ptr);
 	return (PEPE_N);
 }
