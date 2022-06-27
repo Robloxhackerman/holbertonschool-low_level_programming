7bc826a35e6806bb2e736443e0e234cd9cae2b9a
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-27 06:17:07 -0700

    1.0

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable of type struct dog
+ *
+ * @d:
+ * @name: name
+ * @age: age
+ * @char: owner
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
