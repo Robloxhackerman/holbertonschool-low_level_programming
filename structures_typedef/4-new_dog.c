47187e5dbe59cf9d4e0a2283d8d1a7bcaca9f926
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-28 12:03:12 -0700

    4.2

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,56 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ *
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: a dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int PEPE1, PEPE2, PEPE3;
+	dog_t *PEPEPERRON;
+
+	for (PEPE1 = 0; name[PEPE1] != '\0'; PEPE1++)
+	{
+		;
+	}
+	PEPE1++;
+	for (PEPE2 = 0; owner[PEPE2] != '\0'; PEPE2++)
+	{
+		;
+	}
+	PEPE2++;
+
+	PEPEPERRON = malloc(sizeof(dog_t));
+
+	if (PEPEPERRON == 0)
+	{
+		return (NULL);
+	}
+	PEPEPERRON->name = malloc(sizeof(PEPEPERRON->name) * PEPE1);
+
+	if (PEPEPERRON->name == 0)
+	{
+		return (NULL);
+	}
+	for (PEPE3 = 0; PEPE1 > PEPE3; PEPE3++)
+	{
+		PEPEPERRON->name[PEPE3] = name[PEPE3];
+	}
+	PEPEPERRON->age = age;
+	PEPEPERRON->owner = malloc(sizeof(PEPEPERRON->owner) *PEPE2);
+	if (PEPEPERRON->owner == 0)
+	{
+		return (NULL);
+	}
+	for (PEPE3 = 0; PEPE3 < PEPE2; PEPE3++);
+	{
+		PEPEPERRON->owner[PEPE3] = owner[PEPE3];
+	}
+	return (PEPEPERRON);
+}
