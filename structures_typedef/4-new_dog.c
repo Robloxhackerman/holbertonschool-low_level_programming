85067f6c558a4871a00f5a10c25044d7571d094d
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-28 12:08:57 -0700

    4.4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -16,41 +16,36 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *PEPEPERRON;
 
 	for (PEPE1 = 0; name[PEPE1] != '\0'; PEPE1++)
-	{
 		;
-	}
 	PEPE1++;
 	for (PEPE2 = 0; owner[PEPE2] != '\0'; PEPE2++)
-	{
 		;
-	}
 	PEPE2++;
-
 	PEPEPERRON = malloc(sizeof(dog_t));
-
 	if (PEPEPERRON == 0)
 	{
+		free(PEPEPERRON);
 		return (NULL);
 	}
 	PEPEPERRON->name = malloc(sizeof(PEPEPERRON->name) * PEPE1);
-
 	if (PEPEPERRON->name == 0)
 	{
+		free(PEPEPERRON->name);
+		free(PEPEPERRON);
 		return (NULL);
 	}
 	for (PEPE3 = 0; PEPE1 > PEPE3; PEPE3++)
-	{
 		PEPEPERRON->name[PEPE3] = name[PEPE3];
-	}
 	PEPEPERRON->age = age;
-	PEPEPERRON->owner = malloc(sizeof(PEPEPERRON->owner) *PEPE2);
+	PEPEPERRON->owner = malloc(sizeof(PEPEPERRON->owner) * PEPE2);
 	if (PEPEPERRON->owner == 0)
 	{
+		free(PEPEPERRON->owner);
+		free(PEPEPERRON->name);
+		free(PEPEPERRON);
 		return (NULL);
 	}
 	for (PEPE3 = 0; PEPE3 < PEPE2; PEPE3++)
-	{
 		PEPEPERRON->owner[PEPE3] = owner[PEPE3];
-	}
 	return (PEPEPERRON);
 }
