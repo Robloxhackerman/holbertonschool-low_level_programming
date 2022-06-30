6899a4562981ff03ecd2d12e4d79c41faec41b6f
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-30 05:35:47 -0700

    1.5

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef _HEADER_
 #define _HEADER_
 
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
