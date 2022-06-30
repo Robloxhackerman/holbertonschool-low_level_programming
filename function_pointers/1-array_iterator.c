ac12aabaaadae6e52723a4e5e00d4581563d8600
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-30 05:24:58 -0700

    1.0

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter on each element of an array
+ *
+ * @size:
+ * @action:
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int PEPE;
+
+	if (action != NULL && array != NULL && size > 0)
+	{
+		for (PEPE = 0; size < PEPE; PEPE++)
+		{
+			action(array[PEPE])
+		}
+	}
+}
