f5b5f8e24aea65597b2e081ebbb9d022cd59a122
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-30 10:54:42 -0700

    3.2

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -2,4 +2,24 @@
 
 int (*get_op_func(char *s))(int, int)
 {
+	int PEPE;
+	op_t PEPE_POP[] = {
+		{43, op_add},
+		{45, op_sub},
+		{42, op_mul},
+		{47, op_div},
+		{37, op_mod},
+	};
 
+	PEPE = 0;
+
+	while (PEPE_POP[PEPE].op)
+	{
+		if (*(ops[i].op) == *s && s[0] != '\0')
+		{
+			return (PEPE_POP[PEPE].f);
+		}
+		PEPE++;
+	}
+	return (0);
+}
