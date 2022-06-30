6af3a7f5d5bd98b801c3271b9c51f70810ff912b
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-30 10:19:55 -0700

    3.0

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,5 @@
+#include "3-calc.c"
+
+int (*get_op_func(char *s))(int, int)
+{
+
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+int op_add(int a, int b)
+{
+	int PEPESUM;
+
+	PEPESUM = a + b;
+
+	return (PEPESUM);
+}
+
+int op_sub(int a, int b);
+{
+	int PEPERES;
+
+	PEPERES = a - b;
+
+	return (PEPERES);
+}
+
+int op_mul(int a, int b);
+{
+	int PEPEMUL;
+
+	PEPEMUL = a * b;
+
+	return (PEPEMUL);
+}
+
+int op_div(int a, int b);
+{
+	int PEPEDIV;
+
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit();
+	}
+	else
+	{
+		PEPEDIV = a / b;
+		
+		return (PEPEDIV);
+	}
+}
+
+int op_mod(int a, int b)
+{
+	int PEPEROS;
+
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit();
+	}
+	else
+	{
+		PEPEROS = a % b;
+		
+		return (PEPEROS);
+	}
+}
