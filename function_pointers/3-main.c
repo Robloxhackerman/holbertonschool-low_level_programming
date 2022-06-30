f49e58e829efd30ac5046adf2242814b5c8e25b2
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-30 12:37:02 -0700

    3.19

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,12 +12,15 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2] != "+")
+	
+	PEPE_F = get_op_func(argv[2]);
+
+	if (!PEPE_F))
 	{
 		printf("Error\n");
-		exit(98);
+		exit(99);
 	}	
-	PEPE_F = get_op_func(argv[2]);
+	
 	PEPE1 = atoi(argv[1]);
 	PEPE2 = atoi(argv[3]);
 	PEPERESU = PEPE_F(PEPE1, PEPE2);
