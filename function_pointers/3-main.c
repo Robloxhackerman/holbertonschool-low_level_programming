7651775774229b5507e90bc8341ad8577f0a59cc
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-06-30 13:01:55 -0700

    3.27

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - la function
+ * @argc: num of args
+ * @argv: args
+ * Return: always 0
+ */
 int main(int argc, char *argv[])
 {
 	int PEPE1, PEPE2, PEPERESU;
