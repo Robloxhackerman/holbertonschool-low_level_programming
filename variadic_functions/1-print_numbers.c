fbfb6886d5e3e93a339d11ec4bae6355fad85893
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-01 14:16:12 -0700

    1.0

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_numbers - prints numbers
+ *
+ * @separator: separator
+ * @n: num of args
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned PEPE1;
+	int PEPE2;
+	va_list PEPEARG;
+
+	va_start(PEPEARG, int);
+
+	PEPE1 = 0;
+
+	for (PEPE1 = 0; n > PEPE1; PEPE1++)
+	{
+		PEPE2 = va_arg(PEPEARG, int);
+
+		if (separator == NULL)
+		{
+			printf("%d", PEPE2);
+		}
+		else
+		{
+			printf("%d%s", PEPE2, separator);
+		}
+	}
+	printf("\n");
+}
