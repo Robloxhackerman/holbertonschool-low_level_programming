31cf4adabab5b4ce98fe5d88a663c1f1474115b9
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-01 14:22:09 -0700

    1.2

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -20,11 +20,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(PEPEARG, int);
 
 	PEPE1 = 0;
+	PEPE2 = 0;
 
 	for (PEPE1 = 0; n > PEPE1; PEPE1++)
 	{
 		PEPE2 = va_arg(PEPEARG, int);
-
 		if (separator == NULL)
 		{
 			printf("%d", PEPE2);
