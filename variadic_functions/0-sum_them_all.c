be46cfab12b62dd4ffd3e64e3fb12ddd676a0558
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-01 12:59:20 -0700

    0.5

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,6 +1,5 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-#include <stdlib.h>
 
 /**
  * sum_them_All - sum numbers
@@ -13,8 +12,8 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int PEPE1;
         int PEPE2;
-
 	va_list PEPEMETRO;
+
 	va_start(PEPEMETRO, n);
 
 	PEPE2 = 0;
@@ -28,9 +27,7 @@ int sum_them_all(const unsigned int n, ...)
 		for (PEPE1 = 0; n > PEPE1; PEPE1++)
 	
 		{
-		
 			PEPE2 = PEPE2 + va_arg(PEPEMETRO, int);
-	
 		}
 	}
 	va_end(PEPEMETRO);
