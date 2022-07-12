d0b29ae27da9ee4c3a5a8a6d49e726a9848cd97c
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-12 15:29:46 -0700

    2.0

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *listita;
+	unsigned int PEPE1 = 0;
+
+	while (str[PEPE1])
+	{
+		PEPE1++;
+	}
+
+	listita = malloc(sizeof(list_t));
+	if (!listita)
+	{
+		return (NULL);
+	}
+
+	listita->str = strdup(str);
+	listita->len = PEPE1;
+	listita->next = (*head);
+	(*head) = listita;
+
+	return (*head);
+}
