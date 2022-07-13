e20067933537b5e608bfd2fb3160f44c6e070984
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-13 05:44:49 -0700

    2.0

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds nodes
+ *
+ * @head: pointer
+ * @n: elements to add
+ *
+ * Return: pointer
+ */i
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	list_t *listita;
+	unsigned int PEPE1;
+
+	PEPE1 = n;
+
+	listita = malloc(sizeof(list_t));
+
+	if (!listita)
+	{
+		return (NULL);
+	}
+
+	listita->n = PEPE1;
+	listita->next = (*head);
+	(*head) = listita;
+
+	return (*head);
+}
