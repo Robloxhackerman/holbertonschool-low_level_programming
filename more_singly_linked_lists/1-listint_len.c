52bce182b806411e35171d717b3168c395c1b67a
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-13 05:35:32 -0700

    1.0

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - prints amount of elements
+ *
+ * @h: list
+ *
+ * Return: num of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t PEPE1 = 0;
+
+	while (h)
+	{
+		PEPE1++;
+		h = h->next;
+	}
+
+	return (PEPE1);
+}
