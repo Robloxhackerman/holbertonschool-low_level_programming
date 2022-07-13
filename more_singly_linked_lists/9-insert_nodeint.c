fa6e5a456c4e5198f0e3f491afa8c15032f95608
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-13 09:54:09 -0700

    9.algo - betty_god

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,14 @@
 #include "lists.h"
 
+/**
+ * insert_nodeint_at_index - inserts a new node node
+ *
+ * @head: pointer to list
+ * @idx: index
+ * @n: vlues
+ *
+ * Return: address of new node or null if fail
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *listita_P1, *listita_P2;
@@ -14,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	listita_P1->n = n;
 	listita_P2 = *head;
-	
+
 	if (idx == 0)
 	{
 		listita_P1->next = listita_P2;
@@ -28,10 +37,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			return (NULL);
 		}
-		
+
 		listita_P2 = listita_P2->next; 
 	}
-	
+
 	listita_P1->next = listita_P2->next;
 	listita_P2->next = listita_P1;
 
