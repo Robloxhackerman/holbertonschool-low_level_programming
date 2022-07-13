eca26716cf9bf8106eeb85352da86db2d952b5c6
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-13 06:48:11 -0700

    5 try 1

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ *
+ * @head: pointer to list
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *listita;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	while (head != NULL)
+	{
+		listita = head;
+		head = head->next;
+		free(listita);
+	}
+	head = NULL;
+}
