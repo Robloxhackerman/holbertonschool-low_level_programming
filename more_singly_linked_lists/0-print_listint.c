0b9129238841b62318ee58f97499bfa616f7e853
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-13 05:28:31 -0700

    Caaaaaaaaa

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ *
+ * @h: pointer
+ *
+ * Return: elements
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t  PEPE1 = 0;
