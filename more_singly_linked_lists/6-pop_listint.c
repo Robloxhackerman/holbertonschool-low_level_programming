ab92889255b08f3553d0e420ff82db93cd4bcb80
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-13 07:24:17 -0700

    6.0

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+int pop_listint(listint_t **head)
+{
+	listint_t *listita
+	int PEPE;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	
+	listita = *head;
+	PEPE = (*head)->n;
+	*head = (*head)->next;
+
+	free(listita);
+
+	return (PEPE);
+}
