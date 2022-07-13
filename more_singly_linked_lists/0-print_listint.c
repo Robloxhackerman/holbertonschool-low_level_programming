823371041383f3f72414d08cff5f4975d2e3073b
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-13 05:10:43 -0700

    0.0 y README

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+size_t print_listint(const listint_t *h)
+{
+	size_t = PEPE1;
+
+	while (h)
+	{
+		PEPE1++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	
+	return (PEPE1)
+}
