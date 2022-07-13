33bd16999bb69e3eb85e1da30d8e246de25009dd
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-13 08:26:24 -0700

    9.0 - jijijija

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -5,7 +5,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *listita_P1, *listita_P2;
 	unsigned int PEPE1;
 
-	listita = malloc(sizeof(listint_t));
+	listita_P1 = malloc(sizeof(listint_t));
 
 	if (listita == NULL)
 	{
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	
 	listita_P1->next = listita_P2->next;
-	listita_P2-next = listita_P1;
+	listita_P2->next = listita_P1;
 
 	return (listita_P1);
 }
