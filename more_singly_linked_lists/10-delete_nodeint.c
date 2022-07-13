4b70d832bb0ed0a9bf0da2d64999630272a5d04e
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-13 10:25:00 -0700

    10.0

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -1,39 +1,45 @@
 #include "lists.h"
 
+/**
+ * delete_nodeint_at_index - deletes the node at index of a list
+ *
+ * @head: pointer a list
+ * @index: indexo of node
+ *
+ * Return: 1 good, -1 fail
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *listita_P1, *listita_P2;
 	unsigned int PEPE1;
 
-	listita_P1 = malloc(sizeof(listint_t));
+	listita_P1 = *head;
 
 	if (listita_P1 == NULL)
 	{
-		return ();
+		return (-1);
 	}
 
-	listita_P1->n = n;
-	listita_P2 = *head;
-	
-	if (idx == 0)
+	if (index == 0)
 	{
-		listita_P1->next = listita_P2;
-		*head = listita_P1;
-		return (listita_P1);
+		*head = (*head)->next;
+		free(listita_P1);
+		return (1);
 	}
 
-	for (PEPE1 = 0; idx > PEPE1; PEPE1++)
+	for (PEPE1 = 0; (index - 1) > PEPE1; PEPE1++)
 	{
-		if (listita_P2 == NULL || listita_P2->next == NULL)
+		if (listita_P1->next == NULL)
 		{
-			return (NULL);
+			return (-1);
 		}
-		
-		listita_P2 = listita_P2->next; 
-	}
-	
-	listita_P1->next = listita_P2->next;
-	listita_P2->next = listita_P1;
 
+		listita_P1 = listita_P1->next;
+	}
+
+	listita_P2 = listita_P1->next;
+	listita_P1->next = listita_P2->next;
+	free(listita_P2)
+	
 	return (listita_P1);
 }
