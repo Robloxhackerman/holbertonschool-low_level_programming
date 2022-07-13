81e60e5ca865dae1cd92de0f4eca9545043645ac
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-12 17:06:37 -0700

    list

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define _LISTITA_
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct list_s -linked list
@@ -22,5 +23,7 @@ int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
+
+void free_list(list_t *head);
 
 #endif
