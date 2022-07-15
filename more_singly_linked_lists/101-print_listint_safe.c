945a036d0be06e3b47016e9be186613cfc69a6bb
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-15 14:16:43 -0700

    101.1 - el teclado xdd

--- more_singly_linked_lists/101-print_listint_safe.c
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -12,13 +12,13 @@ size_t print_listint_safe(const listint_t *head)
 
 		printf("[%p] %d\n", (void *)head, head->n);
 
-		if (PEPE2 < 0)
+		if (PEPE2 > 0)
 		{
 			head = head->next;
 		}
 		else
 		{
-			printf("[%p] %d\n", (void *)head->next, head->next->n);
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			break;
 		}
 	}
