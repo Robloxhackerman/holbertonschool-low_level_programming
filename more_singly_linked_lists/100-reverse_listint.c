836eaffad85e5c32d4522fbc41255c70f24682a6
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-15 11:53:42 -0700

    100.0 - ehe

---
+++ more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *listita_P1, listita_P2;
+
+	if (head== NULL)
+	{
+		return (NULL);
+	}
+
+	if ((*head)->next == NULL)
+	{
+		return (*head);
+	}
+
+	listita_P1 = *head;
+	*head = NULL;
+
+	while (listita_P1 != NULL)
+	{
+		listita_P2 = listita_P1->next;
+		listita_P1->next = *head;
+		*head = listita_P1;
+		listita_P1 = listita_P2;
+	}
+
+	return (*head);
+}
