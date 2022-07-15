f3e758f5ec17e449fe447407704b544a9fa36209
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-15 12:01:12 -0700

    100.3 - betty pisame

--- more_singly_linked_lists/100-reverse_listint.c
+++ more_singly_linked_lists/100-reverse_listint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * reverse_listint - reverse a list
+ *
+ * @head: pointer to list
+ *
+ * Return: returns first node
+ */
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *listita_P1, *listita_P2;
