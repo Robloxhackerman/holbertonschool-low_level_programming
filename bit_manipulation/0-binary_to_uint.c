5dd60ad2e718d5d8573c2b40658b7881735b1bd8
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-19 05:49:33 -0700

    0.3 - iq +1000

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -2,25 +2,27 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int PEPE = 0, PEPE1 = 0, PEPE2 = 0;
+	unsigned int PEPE = 0, PEPE1 = 0, PEPE_LEN = 0;
 
 	if (b == NULL)
 	{
 		return (0);
-	}
-	PEPE1 = strlen(b);
+	}	
 
-	while (PEPE1--)
+	PEPE_LEN = strlen(b);
+
+	while (b[PEPE1] != '\0')
 	{
 		if (b[PEPE1] != '0' || b[PEPE1] != '1')
 		{
 			return (0);
 		}
-		if (b[PEPE1] == '1')
+		else if (b[PEPE1] == '1')
 		{
-			PEPE = PEPE 1 << PEPE2;
+			PEPE = PEPE + (2^PEPE_LEN)
 		}
-		PEPE2++;
+		PEPE1++;
+		PEPE_LEN--;
 	}
 	return (PEPE);
 }
