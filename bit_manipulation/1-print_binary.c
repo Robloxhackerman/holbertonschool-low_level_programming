9f73aaedef08ee26958a1e86464179190634a6d2
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-19 05:10:06 -0700

    1.0 - coso

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void print_binary(unsigned long int n)
+{
+	unsigned int PEPE_BI, PEPE;
+	int PEPE1;
+
+	while (n != 0)
+	{
+		PEPE = n % 2;
+		n = n / 2;
+		PEPE_BI = PEPE_BI + (PEPE * PEPE1);
+		PEPE1 = PEPE1 * 10;
+	}
+	return (PEPE_BI);
+}
