a00a07c550bda717191a042d095e21b3e7646be7
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-20 16:03:59 -0700

    void pa el main

--- file_io/main.h
+++ file_io/main.h
@@ -13,6 +13,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int copy_text(const char *pepeFile, const char *juanFile);
+void copy_text(const char *pepeFile, const char *juanFile);
 
 #endif
