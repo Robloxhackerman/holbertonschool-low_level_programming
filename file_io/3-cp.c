e7fcb546b29e9dbdb00e237cd0b71be4ad2e7b87
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-20 15:26:52 -0700

    3.1 - try

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,7 +4,7 @@ int main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
-		dptintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	else
