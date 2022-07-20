7e36a4f649633e8f31d5a4dbf8ae1cdc04829e37
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-20 10:47:43 -0700

    3.1 - la funcion

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -6,7 +6,7 @@
  * @text_content: content.
  * Return: 1 success, -1 failure.
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int createMe;
 
