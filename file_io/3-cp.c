ad49dc8f7d7e8b8349041f261ecd57eede3763ee
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-20 16:37:37 -0700

    3.12 - big cambieishon

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
 
 void copy_text(const char *pepeFile, const char *juanFile)
 {
-	int copyMe, createMe, readMe;
+	int copyMe, createMe, readMe, writeMe;
 	char allChars[1024];
 	mode_t modsie = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
@@ -27,13 +27,8 @@ void copy_text(const char *pepeFile, const char *juanFile)
 	
 	createMe = open(juanFile, O_WRONLY | O_TRUNC | O_CREAT, modsie);
 	readMe = read(copyMe, allChars, 1024);
-	write(createMe, allChars, readMe);
+	writeMe = write(createMe, allChars, readMe);
 
-	if (close(copyMe) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", pepeFile);
-		exit(99);
-	}
 	if (createMe == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", juanFile);
@@ -43,5 +38,20 @@ void copy_text(const char *pepeFile, const char *juanFile)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", pepeFile);
 		exit(98);
+	}
+	if (writeMe == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", pepeFile);
+		exit(99);
+	}
+	if (close(createMe) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", createMe);
+		exit(100);
+	}
+	if (close(copyMe) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", copyMe);
+		exit(100);
 	}
 }
