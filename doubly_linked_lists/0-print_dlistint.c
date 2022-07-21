8f2ab8fe546ad36dd1b6cd9b24cb4086bef1d06f
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-21 10:38:58 -0700

    0.2 - betty

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ *
+ * @h: list
+ *
+ * Return: numbers of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	int PEPE1 = 0;
