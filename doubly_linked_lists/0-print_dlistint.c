2ead314ce4e7d14564decfd1049786bdb93784c2
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-21 10:33:48 -0700

    0.0 - try

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int PEPE1;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		PEPE1++;
+		h = h->next;
+	}
+	return (PEPE1);
+}
