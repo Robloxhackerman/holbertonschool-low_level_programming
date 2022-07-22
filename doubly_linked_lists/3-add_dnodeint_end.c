d97bc2595f4ab337e410112e4532b16a4cda7bc6
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-22 06:09:12 -0700

    3.7 - xdddd

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+ *
+ * @head: list
+ * @n: caontent
+ *
+ * Return: address
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *PEPODO = NULL, *PEPOCTUAL = NULL;
+
+	PEPODO = malloc(sizeof(dlistint_t));
+
+	if (PEPODO == NULL)
+	{
+		return (NULL);
+	}
+
+	PEPODO->n = n;
+
+	if (*head)
+	
+	{
+		PEPOCTUAL = *head;
+
+		while(PEPOCTUAL->next != NULL)
+		{
+			PEPOCTUAL = PEPOCTUAL->next;
+		}
+
+		PEPODO->next = NULL;
+		PEPODO->prev = PEPOCTUAL;
+		PEPOCTUAL->next = PEPODO;
+
+		return (PEPODO);
+	}
+	
+	PEPODO->next = *head;
+	PEPODO->prev = NULL;
+	*head = PEPODO;
+	return (*head);
+}
