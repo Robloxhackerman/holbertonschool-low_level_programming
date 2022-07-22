f14d375cf260f0a0b1d3744de523dd41d981ddf7
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-22 16:41:38 -0700

    7.5 - quien pone los nombres de las variables?????

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	while (PEPOCTUAL != NULL)
 	{
-		if (idex == PEPE1)
+		if (idx == PEPE1)
 		{
 			PEPODO->n = n;
 			PEPODO->next = PEPOCTUAL;
