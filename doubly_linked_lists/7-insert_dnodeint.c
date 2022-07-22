574ec26e47805bfa736a0efdcc024644a003bc65
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-22 16:23:08 -0700

    7.0 - creo

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - adds a new node at the index of a dlistint_t list
+ *
+ * @h: list
+ * @idx: index
+ * @n: content
+ *
+ * Return: address or null if fails
+ */
+dlistint_t *add_dnodeint(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *PEPODO = malloc(sizeof(dlistint_t)), *PEPOCTUAL = NULL;
+	unsigned int PEPERGO = dlistint_len(*h), PEPE1 = 0;
+
+	if (PEPODO == NULL)
+	{
+		return (NULL);
+	}
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
+	else if (PEPERGO == idx)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+
+	PEPOCTUAL = *h;
+	
+	while (PEPOCTUAL != NULL)
+	{
+		if (PEPE1 == idx)
+		{
+			PEPODO->n = n;
+			PEPODO->next = PEPOCTUAL;
+			PEPODO->prev = PEPOCTUAL->prev;
+			PEPOCTUAL->prev = PEPODO;
+			PEPOCTUAL = PEPODO;
+			PEPOCTUAL->prev->next = PEPODO;
+			return (PEPODO);
+		}
+		
+		PEPOCTUAL = PEPOCTUAL->next;
+		PEPE1++;
+	}
+	
+	return (PEPOCTUAL);
+}
