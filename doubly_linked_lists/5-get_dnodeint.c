c3cce17486a2c7764d0c0cb6ff0c3b14f3b5218b
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-07-22 06:33:17 -0700

    5.0 - tal vez???

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ *
+ * @head: list
+ * @index: index
+ *
+ * Return: node, or NULL if fails
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *PEPOCTUAL = head;
+	int PEPE1 = 0;
+
+	if (head)
+	{
+		while (PEPOCTUAL != NULL)
+		{
+			if (PEPE1 == index)
+			{
+				return (PEPOCTUAL);
+			}
+
+			PEPOCTUAL = PEPOCTUAL->next;
+			PEPE1++;
+		}
+	}
+
+	return (NULL);
+
+}
