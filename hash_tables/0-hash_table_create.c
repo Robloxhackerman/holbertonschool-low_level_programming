66fc30c452f5417bb1700e7d2a2f399f6a69bc47
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-08-21 12:42:23 -0700

    malloc

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -7,8 +7,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int PEPE1;
 
 	tablita = malloc(sizeof(hash_table_t));
-	arreglito = malloc(sizeof(hash_node_t)* size);
+	arreglito = malloc(sizeof(hash_node_t *) * size);
 
+	if (tablita == NULL)
+	{
+		return (NULL);
+	}
+	if (arreglito == NULL)
+	{
+		return (NULL);
+	}
 	for (PEPE1 = 0; size > PEPE1; PEPE1++)
 	{
 		arreglito[PEPE1] = NULL;
