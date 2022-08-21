06db04a00f5dd9bb092d669e5a61e558a8733caa
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-08-21 13:18:22 -0700

    betty-god

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,5 +1,13 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - gives you the index of a key
+ *
+ * @key: the key
+ * @size: the size
+ *
+ * Return: the index in hash table
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
