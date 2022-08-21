806ca9f3addcf573004360a8686e140c989d6df6
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-08-21 12:36:25 -0700

    try 1

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,6 +1,21 @@
-#include <hash_tables.h>
+#include "hash_tables.h"
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *tablita;
+	hash_node_t **arreglito;
+	int PEPE1;
+
+	tablita = malloc(sizeof(hash_table_t));
+	arreglito = malloc(sizeof(hash_node_t * size));
+
+	for (PEPE1 = 0; size > PEPE1; PEPE1++)
+	{
+		arreglito[PEPE1] = NULL;
+	}
 	
+	tablita->size = size;
+	tablita->array = arreglito;
+
+	return (tablita);
 }
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,6 +1,9 @@
 #ifndef _PEPON_
 #define _PEPON_
 
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
