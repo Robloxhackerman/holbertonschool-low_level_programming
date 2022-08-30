3378da5a426da90b93d370dbb2cc82634d182507
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-08-30 13:37:47 -0700

    try 1

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,46 @@
+#include "search_algos.h"
+
+/**
+ * linear_search -  searches for a value in an array of
+ * integers using the  Binary search algorithm
+ *
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: is the value to search for
+ *
+ * Return: -1 if fail, index success
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t PEPITAD, PABAJO, PARRIBA;
+
+	PABAJO = 0;
+	PARRIBA = size;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	while (PABAJO <= PARRIBA)
+	{
+		PEPITAD = (PABAJO + PARRIBA) / 2;
+
+		printf("Value checked array[%lu] = [%d]\n", PEPE1, array[PEPE1]);
+
+		if (array[PEPITAD] == value)
+		{
+			return (PEPITAD);
+		}
+		if (array[PEPITAD] > value)
+		{
+			PARRIBA = PEPITAD;
+			PEPITAD = (PABAJO + PARRIBA) / 2;
+		}
+		if (array[PEPITAD] < value)
+		{
+			PABAJO = PEPITAD;
+			PEPITAD = (PABAJO + PARRIBA) / 2;
+		}
+	}
+	return (-1);
+}
