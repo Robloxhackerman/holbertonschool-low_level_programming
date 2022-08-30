572d206a16a54183543b75c733fc7993c1604d07
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-08-30 12:51:43 -0700

    aver

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -10,11 +10,12 @@ int linear_search(int *array, size_t size, int value)
 	}
 	for (PEPE1 = 0; size > PEPE1; PEPE1++)
 	{
+		printf("Value checked array[%lu] = [%d]\n", PEPE1, array[PEPE1]);
+
 		if (value == array[PEPE1])
 		{
-			printf("Found %d at index: %lu\n", value, PEPE1);
+			return (PEPE1);
 		}
-		printf("Value checked array[%lu] = [%d]\n", PEPE1, array[PEPE1]);
 	}
 	return (0);
 }
